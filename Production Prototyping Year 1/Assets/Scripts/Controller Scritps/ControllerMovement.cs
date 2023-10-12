f2be81ae270dd40f7ba07e1b421dfc7cc8c51265
RichoJr <rzac1401@gmail.com>
2023-10-12 16:49:20 +1100

    Added Controller Support for player movement

---
+++ Production Prototyping Year 1/Assets/Scripts/Controller Scritps/ControllerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovement : MonoBehaviour
+{
+    [SerializeField] private Transform playerOrientation;
+    [SerializeField] private float movementSpeedBase;
+    [SerializeField] private KeyCode crouchKey;
+
+    private Rigidbody playerRigidbody;
+    private Vector3 movementDirection;
+    private Vector3 playerScaleBase;
+    private Vector3 playerScaleCrouching;
+    private float movementSpeedCalc;
+    private float horizontalInput;
+    private float verticalInput;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+
+        playerScaleBase = transform.localScale;
+        playerScaleCrouching = new Vector3(transform.localScale.x, transform.localScale.y * 0.5f, transform.localScale.z);
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        if (Input.GetKey(crouchKey))
+        {
+            transform.localScale = playerScaleCrouching;
+            movementSpeedCalc = movementSpeedBase * 0.6f;
+        }
+
+        else
+        {
+            transform.localScale = playerScaleBase;
+            movementSpeedCalc = movementSpeedBase;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        movementDirection = playerOrientation.forward * verticalInput + playerOrientation.right * horizontalInput;
+
+        playerRigidbody.AddForce(movementDirection.normalized * movementSpeedCalc, ForceMode.Force);
+    }
+}
